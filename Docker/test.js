36f9514fac8bbc5bd4bf60a3b6397f5d9787d32b
MWiebicke <142303469+MWiebicke@users.noreply.github.com>
2023-09-14 18:02:13 +0200

    Update test.js

--- Docker/test.js
+++ Docker/test.js
@@ -13,14 +13,6 @@ let printObj = function (obj) {
     return string;
 }
 
-async function delVer(){
-    let params = new URLSearchParams(document.location.search);
-    let id = params.get("id");
-const request = await fetch("../../php_eingabe/AdminVeranstaltungDelete.php?veranstaltungs_id="+id);
-window.open("../Kalenderpage/kalender4.html", "_self")
-const data = await request
-}
-
 <script src="/ads.js" type="text/javascript"></script>
 <script type="text/javascript">
 if( document.getElementById('PR44EPxgRGrsoVe') ){
